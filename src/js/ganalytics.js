6ada38f0d9276927679f835215e4d371cc2d25cb
doromones <doromones@gmail.com>
2013-10-14 22:09:55 +0300

    Добавляю сообщения об ошибках

--- src/js/ganalytics.js
+++ src/js/ganalytics.js
@@ -7,3 +7,15 @@ _gaq.push(['_trackPageview']);
 	ga.src = 'https://ssl.google-analytics.com/ga.js';
 	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 })();
+
+/*аналитика ошибок JS*/
+window.onerror = function(msg, url, line) {
+	var preventErrorAlert = true;
+	_gaq.push(['_trackEvent', 'JS Error', msg, navigator.userAgent + ' -> ' + url + " : " + line, 0, true]);
+	return preventErrorAlert;
+};
+
+/*Простенькие сообщения об ошибках jquery*/
+jQuery.error = function (message) {
+	_gaq.push(['_trackEvent', 'jQuery Error', message, navigator.userAgent, 0, true]);
+}
